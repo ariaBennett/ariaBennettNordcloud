a6472dded8c57893744d2b3defac73194173d4f8
Aria Bennett <twoplustwo@gmail.com>
2018-10-27 00:11:15 +0200

    Added getDistance() tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 var assert = require('assert');
 var linkstation = require('../linkstation.js')
-console.log(linkstation);
+
 describe('#getDistance()', function() {
   it('should return 5', function() {
     assert.equal(linkstation.getDistance( [1, 3],[5, 6] ), 5);
+  });
+  it('should return 5', function() {
+    assert.equal(linkstation.getDistance( [5, 6],[1, 3] ), 5);
+  });
+  it('should return 0', function() {
+    assert.equal(linkstation.getDistance( [0, 0],[0, 0] ), 0);
+  });
+  it('should return "8.485281"', function() {
+    assert.equal((linkstation.getDistance( [5, 7],[-1, 1] )).toFixed(6), "8.485281");
   });
 });
